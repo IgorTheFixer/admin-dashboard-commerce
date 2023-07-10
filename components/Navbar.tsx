bb9ca8458cada19736ac980bc15241a6d7edb885
IgorTheFixer <75816411+IgorTheFixer@users.noreply.github.com>
2023-07-10 18:14:09 -0400

    create navbar and skeleton

---
+++ components/Navbar.tsx
@@ -0,0 +1,20 @@
+import { UserButton } from "@clerk/nextjs"
+
+const Navbar = () => {
+  return(
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <div>
+          Store Switch Button
+        </div>
+        <div>
+            Routes
+        </div>
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Navbar
