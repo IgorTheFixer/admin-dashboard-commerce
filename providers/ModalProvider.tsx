6d0d780029d5aaeae8de0c543b4b3cdd06519ce4
IgorTheFixer <75816411+IgorTheFixer@users.noreply.github.com>
2023-07-11 14:26:23 -0400

    finished Settings page and logic

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { StoreModal } from "@/components/modals/StoreModal";
 
 export const ModalProvider = () => {
+  //Mounted logic needed to prevent bad hydration of component
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
